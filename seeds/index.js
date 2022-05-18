a6796b717a111a8c4205e6122c2dee7623eafe54
Fran Sporish <franvvg1@gmail.com>
2022-05-18 19:35:21 +0200

    image upload and cloudinary

--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,18 @@ const seedDB = async () => {
             author:'6279209dae565e023c3c5924',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            price,
+            images: [ 
+                {  
+                    url : "https://res.cloudinary.com/dvzr3jldj/image/upload/v1652719199/YelpCamp/jy1oqjj5pldxaoxcws5e.jpg", 
+                    filename : "YelpCamp/jy1oqjj5pldxaoxcws5e"
+                 },
+                { 
+                    url : "https://res.cloudinary.com/dvzr3jldj/image/upload/v1652719199/YelpCamp/r1y3gmadoa4uo1c5pj4n.jpg", 
+                    filename : "YelpCamp/r1y3gmadoa4uo1c5pj4n" 
+                } 
+            ]
         })
         await camp.save();
     }
